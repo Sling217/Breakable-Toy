c670f7a50bae243c41f6acedd8806b41e355a34b
Scrith <Scrithp2k1@gmail.com>
2023-05-15 13:46:00 -0400

    Better Authentication Routes.

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -6,16 +6,16 @@ const router = new express.Router();
 const clientRoutes = ["/", "/user-sessions/new", "/users/new"];
 const authedClientRoutes = ["/profile"];
 
+router.get(clientRoutes, (req, res) => {
+  res.sendFile(getClientIndexPath());
+});
+
 router.get(authedClientRoutes, (req, res) => {
   if (req.user) {
     res.sendFile(getClientIndexPath());
   } else {
     res.redirect("/user-sessions/new")
   }
-});
-
-router.get(clientRoutes, (req, res) => {
-  res.sendFile(getClientIndexPath());
 });
 
 export default router;
